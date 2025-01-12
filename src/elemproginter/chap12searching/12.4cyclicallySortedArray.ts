cd1d9d820ee223b1fcbe0117d096307bbe939975
Arun Mathews <(none)>
2025-01-11 16:36:50 -0800

    Small cleanup

--- src/elemproginter/chap12searching/12.4cyclicallySortedArray.ts
+++ src/elemproginter/chap12searching/12.4cyclicallySortedArray.ts
@@ -3,9 +3,6 @@ export function findLowestCyclicallySorted(arr: number[]): number {
 	let upper = arr.length - 1;
 	while (lower < upper) {
 		const mid = lower + Math.floor((upper - lower) / 2);
-		console.log(
-			`lower - ${lower}, upper - ${upper}, mid - ${mid}, arr[lower] - ${arr[lower]}, arr[mid] - ${arr[mid]}, arr[upper] - ${arr[upper]}`,
-		);
 		if (arr[mid] > arr[upper]) {
 			lower = mid + 1;
 		} else {
