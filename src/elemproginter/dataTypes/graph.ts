23da7c8ea1407cd1c173e9da397f7c3e0480cc90
Arun Mathews <(none)>
2025-01-20 13:34:44 -0800

    More practice

--- src/elemproginter/dataTypes/graph.ts
+++ src/elemproginter/dataTypes/graph.ts
@@ -49,4 +49,62 @@ export class Graph {
 			this.adjacencyList.forEach((al) => al.delete(v));
 		}
 	}
+
+	getNeighbors(v: string): Set<string> | undefined {
+		if (this.adjacencyList.get(v) != null) {
+			return new Set(this.adjacencyList.get(v));
+		}
+
+		return undefined;
+	}
+}
+
+export function dfsSearch(graph: Graph, start: string): Array<string> {
+	const dfsResult = new Array<string>();
+	dfsSearchHelper(graph, new Set(), start, dfsResult);
+
+	return dfsResult;
+}
+
+function dfsSearchHelper(
+	graph: Graph,
+	visited: Set<string>,
+	start: string,
+	dfs: Array<string>,
+): void {
+	visited.add(start);
+	dfs.push(start);
+	const maybeNeighbors = graph.getNeighbors(start);
+	if (maybeNeighbors != null) {
+		for (const neighbor of maybeNeighbors) {
+			if (!visited.has(neighbor)) {
+				dfsSearchHelper(graph, visited, neighbor, dfs);
+			}
+		}
+	}
+}
+
+export function bfsSearch(graph: Graph, start: string): Array<string> {
+	const visited = new Set<string>();
+	const queue = new Array<string>();
+	const bfsRes = new Array<string>();
+	queue.push(start);
+	visited.add(start);
+	while (queue.length > 0) {
+		const currNode = queue.shift();
+		if (currNode != null) {
+			bfsRes.push(currNode);
+			const maybeNeighbors = graph.getNeighbors(currNode);
+			if (maybeNeighbors != null) {
+				for (const neighbor of maybeNeighbors) {
+					if (!visited.has(neighbor)) {
+						visited.add(neighbor);
+						queue.push(neighbor);
+					}
+				}
+			}
+		}
+	}
+
+	return bfsRes;
 }
